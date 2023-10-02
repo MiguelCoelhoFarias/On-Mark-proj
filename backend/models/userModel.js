5b26bb7d4f42949fd85712685fc7cee9e6d1b349
Miguel Farias <miguelcoelho500@gmail.com>
2023-10-02 20:09:15 -0300

    autenticacao backend concluida gerando tokens jwt em cookies

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import brcypt from "bcryptjs";
 
 // criando um usuario:
 // autenticacao se e um admin ou usuario do sistema(cliete)
@@ -28,6 +29,10 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await brcypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productsRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); //conectando ao mongoDB
 
 const app = express();
+
+// middleware parser:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie middleware:
+app.use(cookieParser());
 
 // testando aplicacao rodando com rotas funcionando:
 app.get("/", (req, res) => {
@@ -16,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
